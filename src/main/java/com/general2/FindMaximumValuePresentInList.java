97ef52859668aa0f4c704376dc383caac193daa3
Vikas <vikash.singh@arohatech.com>
2022-12-17 10:33:52 +0530

    17-Dec-2022

---
+++ src/main/java/com/general2/FindMaximumValuePresentInList.java
@@ -0,0 +1,20 @@
+package com.general2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class FindMaximumValuePresentInList {
+
+    public static void main(String args[]) {
+
+        List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
+
+        int max =  myList.stream()
+                .max(Integer::compare)
+                .get();
+
+        System.out.println(max);
+
+    }
+}
